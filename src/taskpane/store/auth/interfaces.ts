97d6459643f3087d88591ff1416a91de04b53e6f
Julien Aupart <julien.aupart@gmail.com>
2020-01-31 14:44:20 +0100

    style: Simplify login interface

--- src/taskpane/store/auth/interfaces.ts
+++ src/taskpane/store/auth/interfaces.ts
@@ -1,14 +1,17 @@
+import { User } from "../../interfaces";
+
 export const LOGIN = 'LOGIN'
 // export const SET_USER = 'SET_USER'
 
 interface SetTokenAction {
     type: typeof LOGIN
     token: string
+    user: User
 }
 
 export type AuthActionTypes = SetTokenAction // | SomeOtherAction
 
 export interface AuthState {
     token: string
-    user: 
+    user: User
 }
